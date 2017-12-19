a58c054fcf46c9d2241b784c6a064e30f6f6c94e
zyfand <1105564280@qq.com>
2017-12-19 22:51:39 +0800

    bbs-admin inital

---
+++ bbs-admin/src/main/java/com/dldx/BbsAdminApplication.java
@@ -0,0 +1,12 @@
+package com.dldx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbsAdminApplication.class, args);
+	}
+}
